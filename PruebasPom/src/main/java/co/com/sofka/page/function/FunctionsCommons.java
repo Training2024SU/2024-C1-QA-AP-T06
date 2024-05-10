f4ef670e82f9bca4b5be3b64d04bf2d4b0947430
johan <johancfuentes03@gmail.com>
2024-05-10 08:35:21 -0500

    se actualizan funciones comunes y pagina principal

--- PruebasPom/src/main/java/co/com/sofka/page/function/FunctionsCommons.java
+++ PruebasPom/src/main/java/co/com/sofka/page/function/FunctionsCommons.java
@@ -1,7 +1,8 @@
 package co.com.sofka.page.function;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+
+import java.util.List;
 
 public class FunctionsCommons {
     protected final WebDriver driver;
@@ -10,8 +11,57 @@ public class FunctionsCommons {
         this.driver = driver;
     }
 
-    protected void typeInto(By locator, String value){
-        driver.findElement(locator).sendKeys(value);
+    protected void typeInto(By locator, String text) {
+        driver.findElement(locator).sendKeys(text);
+    }
 
+    protected void typeInto(WebElement element, String text) {
+        element.sendKeys(text);
+    }
+
+    protected void clickSelection(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    protected void clickSelection(WebElement element) {
+        element.click();
+    }
+
+    protected void sendKeys(By locator, String text) {
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    protected void sendKeys(WebElement element, String text) {
+        element.sendKeys(text);
+    }
+
+    protected void scrollTo(By locator) {
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].scrollIntoView();", driver.findElement(locator));
+    }
+
+    protected void cleanField(WebElement element) {
+        element.clear();
+    }
+
+    protected void scrollTo(WebElement element) {
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    protected String getText(By locator) {
+        return driver.findElement(locator).getText();
+    }
+
+    protected String getText(WebElement element) {
+        return element.getText();
+    }
+
+    protected String getText(Alert alert) {
+        return alert.getText();
+    }
+
+    protected List<WebElement> findElements(By locator) {
+        return driver.findElements(locator);
     }
 }
