9f1696fb992b197062681eca4188cefde5fe07f5
David Bonelo <bonelodev@gmail.com>
2024-05-10 13:32:04 -0500

    se agrega configuración para firefox

--- PruebasPom/src/test/java/co/com/sofka/setup/WebSetup.java
+++ PruebasPom/src/test/java/co/com/sofka/setup/WebSetup.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 import static co.com.sofka.Constantes.DEMO_QA_URL;
 import static co.com.sofka.Constantes.OPCION_VALIDA;
@@ -23,11 +26,24 @@ public class WebSetup {
             case 2:
                 edgeConfiguration();
                 break;
+            case 3:
+                firefoxConfiguration();
             default:
                 System.out.println(OPCION_VALIDA);
         }
         driver.get(DEMO_QA_URL);
         maximize();
+    }
+
+    private void firefoxConfiguration() {
+        FirefoxOptions fo = new FirefoxOptions();
+        fo.addArguments("-private");
+        fo.addPreference("extensions.allowPrivateBrowsingByDefault", true);
+        FirefoxProfile fp = new FirefoxProfile();
+        fp.setPreference("browser.privatebrowsing.autostart", true);
+        fo.setProfile(fp);
+//                fo.addArguments("-headless");
+        driver = new FirefoxDriver(fo);
     }
 
     private void edgeConfiguration() {
